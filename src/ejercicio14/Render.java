ce7aed73452765f0d8ebf9e44761246280b62b7f
Marcos Janchuk <marcosjanchuk0@gmail.com>
2025-10-06 23:18:26 -0300

    Trabajo en JAVA unidad 5 - POO

---
+++ src/ejercicio14/Render.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio14;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Render {
+    String formato;
+    Proyecto proyecto; // Asociación unidireccional
+}
